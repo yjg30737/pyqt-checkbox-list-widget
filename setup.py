e897ad3c630dc7bd8a3e099b7af79c37630ab346
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 18:30:55 +0900

    Update setup.py to upload this to package

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-checkbox-list-widget',
@@ -9,6 +17,8 @@ setup(
     packages=find_packages(),
     description='PyQt QListWidget for checkable items',
     url='https://github.com/yjg30737/pyqt-checkbox-list-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-tooltip-list-widget>=0.0.1'
